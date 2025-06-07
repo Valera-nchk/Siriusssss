0215c83f55f3f08aca4a711a4a20691b383b11ad
Valera-nchk <vlinter@icloud.com>
2025-06-07 10:14:50 +0300

    конец

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import webbrowser
 import os
 
-html_file = os.path.join(os.path.dirname(__file__), "кодик.html")
+html_file = os.path.join(os.path.dirname(__file__), "index.html")
 
 webbrowser.open(f"file://{html_file}")
